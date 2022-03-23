0bed3bd3698de8848dd1d572904c3c80de1455dd
andreu casals <acasalsr@gmail.com>
2022-03-23 19:12:57 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+const endPoint = "https://tc-todo-2022.herokuapp.com/todos";
+function AfegirTodo({ onTodoAdded }) {
+  const titleRef = useRef();
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = titleRef.current.value;
+        titleRef.current.value = "";
+        fetch(endPoint, {
+          method: "POST",
+          body: JSON.stringify({ title }),
+        })
+          .then((response) => response.json())
+          .then((json) => onTodoAdded(json));
+      }}
+    >
+      <input ref={titleRef} />
+      <input type="Submit" value="Afegir" />
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch(endPoint)
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  }, []);
+
   return (
     <>
-      <h1>hello</h1>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.completed}>{todo.title}</li>
+        ))}
+      </ul>
+      <AfegirTodo onTodoAdded={(todo) => setTodos([...todos, todo])} />
     </>
   );
 }
